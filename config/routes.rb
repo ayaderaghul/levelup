8f7ee572fee2191385f3153be14c963af9847611
chi <linh-chi.nguyen@creditshelf.com>
2019-10-31 22:22:27 +0100

    ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+  root 'landing#index'
+  get '/tutorial', to: 'landing#tutorial'
+  get '/users/sign_in', to: 'home#index'
+  get '/ui', to: 'landing#ui'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
